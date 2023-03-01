90ff115e621fe11cd0629a8336694b946a86c223
yazdan-zeinali <yazdanperspolisi5320@gmail.com>
2023-03-01 18:53:43 +0330

    solve the problem with min and max function

---
+++ min -max.py
@@ -0,0 +1,9 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+min_number = min(number1, number2, number3)
+max_number = max(number1, number2, number3)
+
+print("The smallest number is", min_number)
+print("The largest number is", max_number)
